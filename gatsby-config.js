646dbde08f597bf82bdf7c384a221c5bc763fdf7
Steven Diamante <SDiamante13@gmail.com>
2020-09-25 00:57:46 -0500

    Set up the gatsby-all-in template.

---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path')
+require('dotenv').config({
+  path:
+    process.env.NODE_ENV === 'development'
+      ? path.join(__dirname, '/.env.development')
+      : path.join(__dirname, '/.env.production'),
+})
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-playground',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-plugin-less',
+      options: {
+        javascriptEnabled: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-svgr',
+      options: {
+        svgoConfig: {
+          pretty: true,
+          multipass: true,
+          plugins: [
+            { removeViewBox: false },
+            { removeAttrs: { attrs: '(width|height)' } },
+          ],
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-alias-imports',
+      options: {
+        alias: {
+          '@src': path.join(__dirname, 'src'),
+          '@components': path.join(__dirname, 'src/components'),
+          '@layouts': path.join(__dirname, 'src/components/layouts'),
+          '@pages': path.join(__dirname, 'src/pages'),
+          '@styles': path.join(__dirname, 'src/styles'),
+          '@static': path.join(__dirname, 'static'),
+          '@store': path.join(__dirname, 'src/store'),
+          '@providers': path.join(__dirname, 'src/components/providers'),
+          '@common': path.join(__dirname, 'src/components/common'),
+          '@containers': path.join(__dirname, 'src/components/containers'),
+        },
+      },
+    },
+  ],
+}
